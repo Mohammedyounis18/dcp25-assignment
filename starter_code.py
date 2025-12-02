84c97aa5ad4a2ea46913177a89b8d0321fdc13f3
Mohammedyounis18 <c24372973@mytudublin.ie>
2025-12-02 23:14:30 +0000

    created database table

--- starter_code.py
+++ starter_code.py
@@ -8,3 +8,23 @@ class ABCParser:
     def __init__(self, db_name: str = "tunes.db"):
         self.db_name = db_name
         self.init_database()
+        
+         # Creates the database table 
+    def init_database(self):
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        # Table stores: ID, book number, filename, title, type, key, meter, and ABC notation
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS tunes (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                book_number INTEGER,
+                file_name TEXT,
+                title TEXT,
+                tune_type TEXT,
+                key TEXT,
+                meter TEXT,
+                abc_notation TEXT
+            )
+        ''')
+        conn.commit()
+        conn.close()
